c4d7040eabf69306be75767e431f847adc8ebf3a
Hendrik Lammers <lammers@gmail.com>
2018-10-30 14:27:12 +0800

    Use Dict instead of List to store rates

--- src/elm/Exchange.elm
+++ src/elm/Exchange.elm
@@ -1,12 +1,13 @@
 module Exchange exposing (Exchange, decodeExchange)
 
+import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
 import Time exposing (Posix)
 
 
 type alias Exchange =
-    -- { timestamp : Posix
-    { rates : List Rate
+    { timestamp : Posix
+    , rates : Dict String Float
     }
 
 
@@ -14,25 +15,21 @@ type alias Rate =
     ( String, Float )
 
 
-
--- decodeDate : Decoder Posix
--- decodeDate =
---     Decode.andThen dateFromFloat Decode.float
--- dateFromFloat : Float -> Decoder Posix
--- dateFromFloat date =
---     Decode.succeed (Time.millisToPosix date)
+decodeDate : Decoder Posix
+decodeDate =
+    Decode.andThen dateFromInt Decode.int
 
 
-decodeRates : Decoder (List Rate)
-decodeRates =
-    Decode.keyValuePairs Decode.float
+dateFromInt : Int -> Decoder Posix
+dateFromInt date =
+    Decode.succeed (Time.millisToPosix date)
 
 
 decodeExchange : Decoder Exchange
 decodeExchange =
-    Decode.map Exchange
-        -- (Decode.field "timestamp" decodeDate)
-        (Decode.field "rates" decodeRates)
+    Decode.map2 Exchange
+        (Decode.field "timestamp" decodeDate)
+        (Decode.field "rates" <| Decode.dict Decode.float)
 
 
 
